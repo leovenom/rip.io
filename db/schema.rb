5436550fa55e6e875abd1de2725af5e18c01dbb5
Tamara Montijo <tamara.montijo@gmail.com>
2020-03-02 16:39:28 +0000

    tours and attraction model

--- db/schema.rb
+++ db/schema.rb
@@ -10,10 +10,30 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_03_02_142630) do
+ActiveRecord::Schema.define(version: 2020_03_02_163247) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "attractions", force: :cascade do |t|
+    t.string "name"
+    t.string "address"
+    t.string "description"
+    t.float "latitude"
+    t.float "longitude"
+    t.bigint "user_id"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["user_id"], name: "index_attractions_on_user_id"
+  end
+
+  create_table "tours", force: :cascade do |t|
+    t.bigint "user_id"
+    t.integer "price"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["user_id"], name: "index_tours_on_user_id"
+  end
 
   create_table "users", force: :cascade do |t|
     t.string "email", default: "", null: false
@@ -27,4 +47,6 @@ ActiveRecord::Schema.define(version: 2020_03_02_142630) do
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
 
+  add_foreign_key "attractions", "users"
+  add_foreign_key "tours", "users"
 end
