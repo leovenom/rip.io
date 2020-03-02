ee75f69e0af284fdb54c865a1f75aaac085d5a19
Pedro Ventura <pedro.ventura19g@gmail.com>
2020-03-02 19:30:12 +0000

    wip

--- app/controllers/attractions_controller.rb
+++ app/controllers/attractions_controller.rb
@@ -8,4 +8,21 @@ class AttractionsController < ApplicationController
     @attraction = Attraction.find(params[:id])
   end
 
+  def new
+    @attraction = Attraction.new
+  end
+
+  def create
+    @attraction = Attraction.new(attraction_params)
+
+    if @attraction.save
+      redirect_to "/attractions"
+    else
+      render :new
+    end
+  end
+
+  def attraction_params
+    params.require(:attraction).permit(:name, :address, :description)
+  end
 end
