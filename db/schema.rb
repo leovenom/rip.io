1b687023b464813a0a513211ef95bda48990a038
Tamara Montijo <tamara.montijo@gmail.com>
2020-03-02 16:55:14 +0000

    booking model

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_03_02_163247) do
+ActiveRecord::Schema.define(version: 2020_03_02_164258) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -25,6 +25,15 @@ ActiveRecord::Schema.define(version: 2020_03_02_163247) do
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.index ["user_id"], name: "index_attractions_on_user_id"
+  end
+
+  create_table "bookings", force: :cascade do |t|
+    t.bigint "user_id"
+    t.bigint "tour_id"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["tour_id"], name: "index_bookings_on_tour_id"
+    t.index ["user_id"], name: "index_bookings_on_user_id"
   end
 
   create_table "tours", force: :cascade do |t|
@@ -48,5 +57,7 @@ ActiveRecord::Schema.define(version: 2020_03_02_163247) do
   end
 
   add_foreign_key "attractions", "users"
+  add_foreign_key "bookings", "tours"
+  add_foreign_key "bookings", "users"
   add_foreign_key "tours", "users"
 end
