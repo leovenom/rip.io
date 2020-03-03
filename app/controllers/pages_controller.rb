afea789957fe71d8a55528a0551ca0e626adb7fb
Pedro Ventura <pedro.ventura19g@gmail.com>
2020-03-03 14:22:34 +0000

    link_to

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -3,6 +3,16 @@ class PagesController < ApplicationController
   skip_after_action :verify_authorized, only: :home
 
   def home
-
   end
+
+  def bookings
+    @bookings = current_user.bookings #policy_scope(Booking)
+    authorize @bookings
+  end
+
+  def profile
+    @user = current_user
+    authorize @user
+  end
+
 end
--- config/routes.rb
+++ config/routes.rb
@@ -4,4 +4,7 @@ Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 
   resources :attractions
+
+
+  get '/profile', to: 'pages#profile'
 end
