22138ab7361e025419415b1b60715122bc999671
Tamara Montijo <tamara.montijo@gmail.com>
2020-03-05 12:15:19 +0000

    user can update to guide

---
+++ app/controllers/users_controller.rb
@@ -0,0 +1,8 @@
+class UsersController < ApplicationController
+    def update_to_guide
+    @user = current_user
+    @user.guide!
+    authorize @user
+    redirect_to profile_path
+  end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -12,4 +12,5 @@ Rails.application.routes.draw do
   get '/profile', to: 'pages#profile'
   get '/be_a_guide', to: 'pages#be_a_guide'
   delete '/my_bookings', to: 'bookings#destroy', as: 'my_bookings_destroy'
+  patch 'update_to_guide', to: 'users#update_to_guide', as: 'update_to_guide'
 end
