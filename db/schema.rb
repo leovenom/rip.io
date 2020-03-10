dc2ed8b10c90199df8631c1f3a118b0e49f76c37
Pedro Ventura <pedro.ventura19g@gmail.com>
2020-03-10 10:34:25 +0000

    text appear and adding flatpickr

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_03_09_174516) do
+ActiveRecord::Schema.define(version: 2020_03_10_103139) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -92,6 +92,7 @@ ActiveRecord::Schema.define(version: 2020_03_09_174516) do
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.string "name"
+    t.string "date"
     t.index ["user_id"], name: "index_tours_on_user_id"
   end
 
