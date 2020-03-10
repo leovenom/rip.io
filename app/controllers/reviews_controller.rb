59dec5276c0e5b244d25095f7b663ad695995bca
Ravinder Deol <ravinder.r.deol@gmail.com>
2020-03-10 13:23:00 +0000

    once a tour is now created the review system should work

--- app/controllers/reviews_controller.rb
+++ app/controllers/reviews_controller.rb
@@ -1,5 +1,6 @@
 class ReviewsController < ApplicationController
   before_action :find_tour
+  before_action :find_review, only [:edit, :update, :destroy]
 
   def new
     @review = Review.new
@@ -17,6 +18,22 @@ class ReviewsController < ApplicationController
     end
   end
 
+  def edit
+  end
+
+  def destroy
+    @review.destroy
+    redirect_to tour_path(@tour)
+  end
+
+  def update
+    if @review.update(review_params)
+      redirect_to tour_path(@tour)
+    else
+      render 'edit'
+    end
+  end
+
   private
 
   def review_params
@@ -25,5 +42,9 @@ class ReviewsController < ApplicationController
 
   def find_tour
     @tour = Tour.find(params[:tour_id])
+  end
+
+  def find_review
+    @review = Review.find(params[:id])
   end
 end
